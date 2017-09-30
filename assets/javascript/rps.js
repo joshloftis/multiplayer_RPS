6cc3602844b3bba9556a90ef248a0aa4491ab04d
Josh Loftis <jshlfts32@gmail.com>
2017-09-29 22:47:19 -0400

    basic logic, needs lots of work

--- assets/javascript/rps.js
+++ assets/javascript/rps.js
@@ -1,38 +1,55 @@
+//Firebase Globals
+//-----------------------------------------------------------------------------------------------------------------------------
+//
 var config = {
-  apiKey: "AIzaSyDyumcsYscDN3FeA7dRk5NPP06yeiini2w",
-  authDomain: "rock-paper-scissors-73f3c.firebaseapp.com",
-  databaseURL: "https://rock-paper-scissors-73f3c.firebaseio.com",
-  projectId: "rock-paper-scissors-73f3c",
+  apiKey: "AIzaSyDDvsswWgJUcWOuH_obLC_ZDQzs239drBI",
+  authDomain: "rock-paper-scissors-mp-game-gt.firebaseapp.com",
+  databaseURL: "https://rock-paper-scissors-mp-game-gt.firebaseio.com",
+  projectId: "rock-paper-scissors-mp-game-gt",
   storageBucket: "",
-  messagingSenderId: "622403492860"
+  messagingSenderId: "571149025620"
 };
 firebase.initializeApp(config);
 
+var db = firebase.database();
+var currentUsers = db.ref('/users');
+var consRef = db.ref("/connections");
+var conInfo = db.ref(".info/connected");
 var choices = ["rock", "paper", "scissors"];
-var user1Ans;
-var user2Ans;
-var user1Wins = 0;
-var user1Losses= 0;
-var user2Wins = 0;
-var user2Losses = 0;
-var ties = 0;
-
-function evalGame() {
-  if ((user1Ans === "rock") || (user1Ans === "paper") || (user1Ans === "scissors")) {
-    if ((user1Ans === "rock") && (user2Ans === "scissors")) {
-      wins++;
-    } else if ((user1Ans === "rock") && (user2Ans === "paper")) {
-      losses++;
-    } else if ((user1Ans === "scissors") && (user2Ans === "rock")) {
-      losses++;
-    } else if ((user1Ans === "scissors") && (user2Ans === "paper")) {
-      wins++;
-    } else if ((user1Ans === "paper") && (user2Ans === "rock")) {
-      wins++;
-    } else if ((user1Ans === "paper") && (user2Ans === "scissors")) {
-      losses++;
-    } else if (user1Ans === user2Ans) {
-      ties++;
-    }
+var users = {
+  userOne: {
+    name: 'Josh',
+    id: 0,
+    wins: 0,
+    losses: 0,
+    currentPlay: ''
+  },
+  userTwo: {
+    name: '',
+    id: 0,
+    wins: 0,
+    losses: 0,
+    currentPlay: ''
   }
-}
+};
+
+currentUsers.set(users);
+
+conInfo.on('value', function(snap) {
+  if (snap.val()){
+    var con = consRef.push(true);
+    con.onDisconnect().remove();
+  }
+});
+
+consRef.on('value', function(snap) {
+  if (snap.numChildren() == 1) {
+    console.log("You're the first one here");
+  } else if (snap.numChildren() == 2) {
+    console.log("You're the second one here");
+  }
+});
+
+currentUsers.on('value', function(snap) {
+  console.log(snap.val().userOne.name);
+});
